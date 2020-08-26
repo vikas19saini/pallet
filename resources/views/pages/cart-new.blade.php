08487794db159c72fa32465d2bb488e08e66df90
vikas saini <vikas.saini@prozo.com>
2020-08-27 01:12:19 +0530

    fixes

--- resources/views/pages/cart-new.blade.php
+++ resources/views/pages/cart-new.blade.php
@@ -3,58 +3,58 @@
 @section('content')
 
 <section class="cart_section">
-  <div class="container">
-    <div class="row">
-      <div class="cart_area">
-        <div class="cart_left">
-            <div class="cart_first">
-                <h2 style="margin-bottom: 0px">My Bag (  <count id="cart-total-item-count"> {{ count($cartItems)}} </count>  item)  </h2>
-            </div>
-
-            @foreach($cartItems as $item)
-                <div class="cart_first" id="cart-item-block-{{ $item->id }}">
-                    <div class="cart_img">
-                        <img src="{{ url('img/product-images/' . $item->product->primary_image) }}" class="img-responsive">
+    <div class="container">
+        <div class="row">
+            <div class="cart_area">
+                <div class="cart_left">
+                    <div class="cart_first">
+                        <h2 style="margin-bottom: 0px">My Bag ( <count id="cart-total-item-count"> {{ count($cartItems)}} </count> item) </h2>
                     </div>
-                    <div class="cart_content">
-                    <h3> 
-                        {{ $item->product->title }} <span>({{ $item->product->product_key }})</span> 
-                        <b>
-                            {{ $item->order_type == "SAMPLE" ? "3 Pieces" : $item->quantity." Pieces"}}
-                        </b>
-                    </h3>
-                    <p>
-                        {{ $item->product->tagline }} | <strong>{{ $item->order_type}}</strong>
-                    </p>
-                    <p>Fabric: {{ $item->fabrics->name }}</p>
-                    <span style="cursor: pointer" onclick="removeItemFrommCart({{ $item->id }})">
-                        <i class="fa fa-trash"> </i> Remove &nbsp; 
-                    </span>
-                    <span>
-                        <b id="cart-item-price-{{ $item->id }}" val="{{ $item->amount }}">
-                            $ {{ $item->changeCurrencyFormat($item->amount, '$') }}
-                        </b>   
-                    </span>
+
+                    @foreach($cartItems as $item)
+                    <div class="cart_first" id="cart-item-block-{{ $item->id }}">
+                        <div class="cart_img">
+                            <img src="{{ url('img/product-images/' . $item->product->primary_image) }}" class="img-responsive">
+                        </div>
+                        <div class="cart_content">
+                            <h3>
+                                {{ $item->product->title }} <span>({{ $item->product->product_key }})</span>
+                                <b>
+                                    {{ $item->order_type == "SAMPLE" ? "3 Pieces" : $item->quantity." Pieces"}}
+                                </b>
+                            </h3>
+                            <p>
+                                {{ $item->product->tagline }} | <strong>{{ $item->order_type}}</strong>
+                            </p>
+                            <p>Fabric: {{ $item->fabrics->name }}</p>
+                            <span style="cursor: pointer" onclick="removeItemFrommCart({{ $item->id }})">
+                                <i class="fa fa-trash"> </i> Remove &nbsp;
+                            </span>
+                            <span>
+                                <b id="cart-item-price-{{ $item->id }}" val="{{ $item->amount }}">
+                                    $ {{ $item->changeCurrencyFormat($item->amount, '$') }}
+                                </b>
+                            </span>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+                <div class="cart_right">
+                    <div class="cart_total">
+                        <h2>Total</h2>
+                        <p>Subtotal <span id="cart-subtotal-price"></span> </p>
+                        <p class="total_price">
+                            Total (Tax Included) <span id="cart-total-price"></span> </p>
+                        <button>
+                            <a href="{{ url('/cart/address') }}">GO TO CHECKOUT</a>
+                            <!--                    <a href="/cart/address" onclick="return verifyCart()">GO TO CHECKOUT</a>-->
+                        </button>
+                        <span id="cart-empty-err-msg" style="color: red;display:none"> No Item In cart </span>
                     </div>
                 </div>
-            @endforeach
+            </div>
         </div>
-        <div class="cart_right">
-           <div class="cart_total">
-              <h2>Total</h2>
-              <p>Subtotal <span id="cart-subtotal-price"></span> </p>
-              <p class="total_price">
-                Total (Tax Included) <span id="cart-total-price"></span> </p>
-                <button>
-                    <a href="/cart/address">GO TO CHECKOUT</a>
-<!--                    <a href="/cart/address" onclick="return verifyCart()">GO TO CHECKOUT</a>-->
-                </button>
-                <span id="cart-empty-err-msg" style="color: red;display:none"> No Item In cart </span>
-           </div>
-        </div>
-      </div>
     </div>
-  </div>  
 </section>
 
 
@@ -63,19 +63,18 @@
 
 @endsection
 
-@section('pageLevelJS')       
+@section('pageLevelJS')
 
-    <script>
-
-    function calculatePrice(){
+<script>
+    function calculatePrice() {
         var price = 0;
-        var cart_item_length = 0; 
-        $('[id^="cart-item-price-"]').each( function(index,item) { 
-            price += parseFloat( $(item).attr('val') ); 
-            ++cart_item_length; 
+        var cart_item_length = 0;
+        $('[id^="cart-item-price-"]').each(function(index, item) {
+            price += parseFloat($(item).attr('val'));
+            ++cart_item_length;
         });
 
-        $("#cart-total-item-count").html( cart_item_length );
+        $("#cart-total-item-count").html(cart_item_length);
         $("#cart-total-price").html("$" + price);
         $("#cart-subtotal-price").html("$" + price);
 
@@ -86,7 +85,7 @@
             style: 'currency',
             currency: '$',
             minimumFractionDigits: 2
-        }); 
+        });
 
         formatter.format(1000) // "$1,000.00"
         formatter.format(10) // "$10.00"
@@ -94,37 +93,37 @@
 
     }
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         calculatePrice();
 
-    }); 
+    });
 
 
     function removeItemFrommCart(cartId) {
         $.ajax({
-                method: 'POST',
-                url: '{{ url('ajax/cart/') }}/'+ cartId +'/delete',
-                data: { cartId: cartId, _token: '{{ csrf_token() }}' },
-                success: function(data) {
-                    $("#cart-item-block-"+cartId).remove(); 
-                    calculatePrice(); 
-                    // location.reload();
-                }
-        }); 
+            method: 'POST',
+            url: '{{ url('
+            ajax / cart / ') }}/' + cartId + '/delete',
+            data: {
+                cartId: cartId,
+                _token: '{{ csrf_token() }}'
+            },
+            success: function(data) {
+                $("#cart-item-block-" + cartId).remove();
+                calculatePrice();
+                // location.reload();
+            }
+        });
     }
 
-    function changeCheckoutButtonStatus()
-    {
-        if( $('[id^="cart-item-price-"]').length == 0 ) 
-        {
-            $("#cart-empty-err-msg").show(); 
+    function changeCheckoutButtonStatus() {
+        if ($('[id^="cart-item-price-"]').length == 0) {
+            $("#cart-empty-err-msg").show();
             return false;
         }
 
-        return true; 
+        return true;
     }
-
-    </script>
+</script>
 
 @endsection
-
--- storage/framework/views/49fba3386e6204295b5cbf3c30f77fea44c9c15b.php
+++ storage/framework/views/49fba3386e6204295b5cbf3c30f77fea44c9c15b.php
@@ -1,60 +1,60 @@
 <?php $__env->startSection('content'); ?>
 
 <section class="cart_section">
-  <div class="container">
-    <div class="row">
-      <div class="cart_area">
-        <div class="cart_left">
-            <div class="cart_first">
-                <h2 style="margin-bottom: 0px">My Bag (  <count id="cart-total-item-count"> <?php echo e(count($cartItems)); ?> </count>  item)  </h2>
-            </div>
-
-            <?php $__currentLoopData = $cartItems; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <div class="cart_first" id="cart-item-block-<?php echo e($item->id); ?>">
-                    <div class="cart_img">
-                        <img src="<?php echo e(url('img/product-images/' . $item->product->primary_image)); ?>" class="img-responsive">
+    <div class="container">
+        <div class="row">
+            <div class="cart_area">
+                <div class="cart_left">
+                    <div class="cart_first">
+                        <h2 style="margin-bottom: 0px">My Bag ( <count id="cart-total-item-count"> <?php echo e(count($cartItems)); ?> </count> item) </h2>
                     </div>
-                    <div class="cart_content">
-                    <h3> 
-                        <?php echo e($item->product->title); ?> <span>(<?php echo e($item->product->product_key); ?>)</span> 
-                        <b>
-                            <?php echo e($item->order_type == "SAMPLE" ? "3 Pieces" : $item->quantity." Pieces"); ?>
 
-                        </b>
-                    </h3>
-                    <p>
-                        <?php echo e($item->product->tagline); ?> | <strong><?php echo e($item->order_type); ?></strong>
-                    </p>
-                    <p>Fabric: <?php echo e($item->fabrics->name); ?></p>
-                    <span style="cursor: pointer" onclick="removeItemFrommCart(<?php echo e($item->id); ?>)">
-                        <i class="fa fa-trash"> </i> Remove &nbsp; 
-                    </span>
-                    <span>
-                        <b id="cart-item-price-<?php echo e($item->id); ?>" val="<?php echo e($item->amount); ?>">
-                            $ <?php echo e($item->changeCurrencyFormat($item->amount, '$')); ?>
+                    <?php $__currentLoopData = $cartItems; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <div class="cart_first" id="cart-item-block-<?php echo e($item->id); ?>">
+                        <div class="cart_img">
+                            <img src="<?php echo e(url('img/product-images/' . $item->product->primary_image)); ?>" class="img-responsive">
+                        </div>
+                        <div class="cart_content">
+                            <h3>
+                                <?php echo e($item->product->title); ?> <span>(<?php echo e($item->product->product_key); ?>)</span>
+                                <b>
+                                    <?php echo e($item->order_type == "SAMPLE" ? "3 Pieces" : $item->quantity." Pieces"); ?>
 
-                        </b>   
-                    </span>
+                                </b>
+                            </h3>
+                            <p>
+                                <?php echo e($item->product->tagline); ?> | <strong><?php echo e($item->order_type); ?></strong>
+                            </p>
+                            <p>Fabric: <?php echo e($item->fabrics->name); ?></p>
+                            <span style="cursor: pointer" onclick="removeItemFrommCart(<?php echo e($item->id); ?>)">
+                                <i class="fa fa-trash"> </i> Remove &nbsp;
+                            </span>
+                            <span>
+                                <b id="cart-item-price-<?php echo e($item->id); ?>" val="<?php echo e($item->amount); ?>">
+                                    $ <?php echo e($item->changeCurrencyFormat($item->amount, '$')); ?>
+
+                                </b>
+                            </span>
+                        </div>
+                    </div>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </div>
+                <div class="cart_right">
+                    <div class="cart_total">
+                        <h2>Total</h2>
+                        <p>Subtotal <span id="cart-subtotal-price"></span> </p>
+                        <p class="total_price">
+                            Total (Tax Included) <span id="cart-total-price"></span> </p>
+                        <button>
+                            <a href="<?php echo e(url('/cart/address')); ?>">GO TO CHECKOUT</a>
+                            <!--                    <a href="/cart/address" onclick="return verifyCart()">GO TO CHECKOUT</a>-->
+                        </button>
+                        <span id="cart-empty-err-msg" style="color: red;display:none"> No Item In cart </span>
                     </div>
                 </div>
-            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </div>
         </div>
-        <div class="cart_right">
-           <div class="cart_total">
-              <h2>Total</h2>
-              <p>Subtotal <span id="cart-subtotal-price"></span> </p>
-              <p class="total_price">
-                Total (Tax Included) <span id="cart-total-price"></span> </p>
-                <button>
-                    <a href="/cart/address">GO TO CHECKOUT</a>
-<!--                    <a href="/cart/address" onclick="return verifyCart()">GO TO CHECKOUT</a>-->
-                </button>
-                <span id="cart-empty-err-msg" style="color: red;display:none"> No Item In cart </span>
-           </div>
-        </div>
-      </div>
     </div>
-  </div>  
 </section>
 
 
@@ -63,19 +63,18 @@
 
 <?php $__env->stopSection(); ?>
 
-<?php $__env->startSection('pageLevelJS'); ?>       
+<?php $__env->startSection('pageLevelJS'); ?>
 
-    <script>
-
-    function calculatePrice(){
+<script>
+    function calculatePrice() {
         var price = 0;
-        var cart_item_length = 0; 
-        $('[id^="cart-item-price-"]').each( function(index,item) { 
-            price += parseFloat( $(item).attr('val') ); 
-            ++cart_item_length; 
+        var cart_item_length = 0;
+        $('[id^="cart-item-price-"]').each(function(index, item) {
+            price += parseFloat($(item).attr('val'));
+            ++cart_item_length;
         });
 
-        $("#cart-total-item-count").html( cart_item_length );
+        $("#cart-total-item-count").html(cart_item_length);
         $("#cart-total-price").html("$" + price);
         $("#cart-subtotal-price").html("$" + price);
 
@@ -86,7 +85,7 @@
             style: 'currency',
             currency: '$',
             minimumFractionDigits: 2
-        }); 
+        });
 
         formatter.format(1000) // "$1,000.00"
         formatter.format(10) // "$10.00"
@@ -94,39 +93,38 @@
 
     }
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         calculatePrice();
 
-    }); 
+    });
 
 
     function removeItemFrommCart(cartId) {
         $.ajax({
-                method: 'POST',
-                url: '<?php echo e(url('ajax/cart/')); ?>/'+ cartId +'/delete',
-                data: { cartId: cartId, _token: '<?php echo e(csrf_token()); ?>' },
-                success: function(data) {
-                    $("#cart-item-block-"+cartId).remove(); 
-                    calculatePrice(); 
-                    // location.reload();
-                }
-        }); 
+            method: 'POST',
+            url: '<?php echo e(url('
+            ajax / cart / ')); ?>/' + cartId + '/delete',
+            data: {
+                cartId: cartId,
+                _token: '<?php echo e(csrf_token()); ?>'
+            },
+            success: function(data) {
+                $("#cart-item-block-" + cartId).remove();
+                calculatePrice();
+                // location.reload();
+            }
+        });
     }
 
-    function changeCheckoutButtonStatus()
-    {
-        if( $('[id^="cart-item-price-"]').length == 0 ) 
-        {
-            $("#cart-empty-err-msg").show(); 
+    function changeCheckoutButtonStatus() {
+        if ($('[id^="cart-item-price-"]').length == 0) {
+            $("#cart-empty-err-msg").show();
             return false;
         }
 
-        return true; 
+        return true;
     }
-
-    </script>
+</script>
 
 <?php $__env->stopSection(); ?>
-
-
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
